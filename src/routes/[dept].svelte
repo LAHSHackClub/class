6647442e9952730deaf2e375e58a8f84ac259072
SDBagel <sdbagel@gmail.com>
2022-01-05 15:10:14 -0800

    Add department pathway mock

--- src/routes/[dept].svelte
+++ src/routes/[dept].svelte
@@ -21,4 +21,86 @@
   export let dept;
 </script>
 
-<h1>Pathway: {dept}</h1>
+<h1>Pathway: <span>{dept}</span></h1>
+<div class="pathway">
+  <section>
+    <h2>Introductory</h2>
+    <article>
+      <h3>Intro to {dept}</h3>
+      <p>Here is a brief description of the class that gives a general overview</p>
+    </article>
+    <article>
+      <h3>Intro to Engineering & Design</h3>
+      <p>Here is a brief description of the class that gives a general overview</p>
+    </article>
+    <article>
+      <h3>Intro to Engineering & Design</h3>
+      <p>Here is a brief description of the class that gives a general overview</p>
+    </article>
+  </section>
+  <section>
+    <h2>Year 1</h2>
+    <article>
+      <h3>Intro to Engineering & Design</h3>
+      <p>Here is a brief description of the class that gives a general overview</p>
+    </article>
+    <article>
+      <h3>Intro to Engineering & Design</h3>
+      <p>Here is a brief description of the class that gives a general overview</p>
+    </article>
+  </section>
+  <section>
+    <h2>Year 2</h2>
+    <article>
+      <h3>Intro to Engineering & Design</h3>
+      <p>Here is a brief description of the class that gives a general overview</p>
+    </article>
+  </section>
+  <section>
+    <h2>Year 3</h2>
+    <article>
+      <h3>AP Psychology</h3>
+    </article>
+  </section>
+</div>
+
+<style lang="scss">
+  h1 span {
+    text-transform: uppercase;
+  }
+
+  .pathway {
+    display: flex;
+    column-gap: 50px;
+    padding-bottom: 50px;
+    overflow-x: auto;
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    row-gap: 15px;
+    min-width: 320px;
+
+    h2 {
+      font-size: 1rem;
+    }
+
+    article {
+      background-color: #237;
+      border-radius: 5px;
+      padding: 15px 20px;
+      font-size: 0.7em;
+
+      h3 {
+        margin: 0;
+        text-transform: uppercase;
+      }
+      p {
+        color: var(--text-secondary);
+        margin-top: 5px;
+        margin-bottom: 0;
+      }
+    }
+  }
+</style>
