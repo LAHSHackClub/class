44aef221a670b8eec3ac8a4eb0d01baff9c7f023
SDBagel <sdbagel@gmail.com>
2022-01-04 22:10:08 -0800

    Style and mock sidebar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,9 +3,10 @@
 
 <style lang="scss" global>
 	:root {
+		font-family: "Poppins", sans-serif;
 		font-size: 20px;
 	}
-	
+
 	body {
 		margin: 0;
 	}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,19 @@
 <div class="split">
 	<section class="sidebar">
 		<div class="container">
-			<h2>Classes</h2>
-			<h3>Saved</h3>
+			<h2>LAHS Classes</h2>
+			<p class="sublinks">about ⋄ lahs.+ ⋄ privacy</p>
+			<h3>Saved Lists</h3>
+			<a href="">2021 - 2022</a>
 			<h3>Departments</h3>
+			<div class="links">
+				<a href="">All Classes</a>
+				<a href="">Electives</a>
+				<a href="">English</a>
+				<a href="">Fine Arts</a>
+				<a href="">Math</a>
+				<a href="">Science</a>
+			</div>
 		</div>
 	</section>
 	<section class="page">
@@ -27,14 +37,46 @@
 		flex: 1 1;
 
 		.container {
-			max-width: 300px;
+			width: 300px;
 			padding: 30px;
+		}
+
+		.sublinks {
+			font-size: 0.8em;
+		}
+
+		h3 {
+			margin-top: 30px;
+			margin-bottom: 10px;
+		}
+
+		.links {
+			display: flex;
+			flex-direction: column;
+			row-gap: 10px;
+		}
+
+		a {
+			border: 2px solid #339;
+			border-radius: 5px;
+			color: #ddf;
+			display: block;
+			font-size: 0.9em;
+			text-decoration: none;
+			padding: 8px 30px;
+			transition-duration: 0.1s;
+
+			&:hover {
+				background-color: #44c;
+				border-color: #44c;
+				color: #eef;
+			}
 		}
 	}
 
 	.page {
-		background-color: #222;
+		background-color: #223;
 		flex: 2 1;
-		padding: 30px;
+		padding: 30px 60px;
 	}
 </style>
