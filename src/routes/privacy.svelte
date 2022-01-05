7258102431fc69ad38f685a914a7c9629721deb3
SDBagel <sdbagel@gmail.com>
2022-01-05 15:22:46 -0800

    Update privacy.svelte

--- src/routes/privacy.svelte
+++ src/routes/privacy.svelte
@@ -3,9 +3,11 @@
 <p><small>This privacy policy and data usage report is effective for <b>classes.lahs.plus</b>.</small></p>
 <p>. . .</p>
 <h3>A LAHS Hack Club Project</h3>
-<p>Classes (classes.lahs.plus) is run by Los Altos High School's Hack Club. We make a wide variety of software and tools for the people of LAHS - you might have seen some of our work around. As a school association and associated with the Hack Club nonprofit organization, we run this site (and associated expenses, like server costs) out of our club account. If you are interested in learning more about our web projects, <a href="https://get.lahs.club">check out our GET website program for the school.</a></p>
+<p>Classes (classes.lahs.plus) is run by Los Altos High School's Hack Club. We make a wide variety of software and tools for the people of LAHS - you might have seen some of our work around!</p>
+<p>If you are interested in learning more about us, <a href="https://get.lahs.club">check out our GET program</a> or <a href="https://lahs.club">our main page.</a></p>
 <h3>App Data Usage</h3>
-<p>This website features a "save" feature that allows you to save classes to "lists" for future reference. In order for this feature to work, we use your web browser's built-in storage functionality. We do not upload or have access to any of the data you enter into this app. Therefore, if you clear cache or storage in your browser, your data on this site will be permanently deleted. Likewise, it is currently not possible to synchronize actions made with another device. Other basic information may be logged to monitor web traffic and maintain app health for all users. No personally identifiable information is collected.</p>
+<p>This website features a "save" feature that allows you to save classes to "lists" for future reference. In order for this feature to work, we use your web browser's built-in storage functionality. We do not upload or have access to any of the data you enter into this app. Therefore, if you clear cache or storage in your browser, your data on this site will be permanently deleted. Likewise, it is currently not possible to synchronize actions made with another device.</p>
+<p>Other basic information may be logged to monitor web traffic and maintain app health for all users. No personally identifiable information is collected.</p>
 <h3>MVLA Data Compliance</h3>
 <p>Because we do not collect any personally identifiable information, we comply with the MVLA district policy on data management.</p>
 
