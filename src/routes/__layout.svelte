0133014b0efa092892307aabe9db3b6770f09f80
SDBagel <sdbagel@gmail.com>
2022-01-04 21:29:44 -0800

    Add split bar layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+
+<slot />
+
+<style lang="scss" global>
+	:root {
+		font-size: 20px;
+	}
+	
+	body {
+		margin: 0;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="split">
+	<section class="sidebar">
+		<div class="container">
+			<h2>Classes</h2>
+			<h3>Saved</h3>
+			<h3>Departments</h3>
+		</div>
+	</section>
+	<section class="page">
+		<h1>Department Name</h1>
+	</section>
+</div>
+
+<style lang="scss">
+	.split {
+		color: #fff;
+		display: flex;
+		width: 100%;
+		min-height: 100vh;
+	}
+
+	.sidebar {
+		display: flex;
+		justify-content: flex-end;
+		background-color: #001;
+		flex: 1 1;
+
+		.container {
+			max-width: 300px;
+			padding: 30px;
+		}
+	}
+
+	.page {
+		background-color: #222;
+		flex: 2 1;
+		padding: 30px;
+	}
+</style>
