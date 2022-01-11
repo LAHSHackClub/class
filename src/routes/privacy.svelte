1025f75b0b691caf9f2efa20d808c9207c0ba4b5
SDBagel <sdbagel@gmail.com>
2022-01-10 22:20:53 -0800

    Update privacy, data sourcing explanation

--- src/routes/privacy.svelte
+++ src/routes/privacy.svelte
@@ -1,18 +1,22 @@
 
 <h1>Privacy Policy</h1>
-<p><small>This privacy policy and data usage report is effective for <b>classes.lahs.plus</b>.</small></p>
+<p><small>This privacy policy and data usage report is effective as of January 10, 2022.</small></p>
 <p>. . .</p>
 <h3>A LAHS Hack Club Project</h3>
 <p>Classes (classes.lahs.plus) is run by Los Altos High School's Hack Club. We make a wide variety of software and tools for the people of LAHS - you might have seen some of our work around!</p>
 <p>If you are interested in learning more about us, <a href="https://get.lahs.club">check out our GET program</a> or <a href="https://lahs.club">our main page.</a></p>
 <h3>App Data Usage</h3>
-<p>This website features a "save" feature that allows you to save classes to "lists" for future reference. In order for this feature to work, we use your web browser's built-in storage functionality. We do not upload or have access to any of the data you enter into this app. Therefore, if you clear cache or storage in your browser, your data on this site will be permanently deleted. Likewise, it is currently not possible to synchronize actions made with another device.</p>
-<p>Other basic information may be logged to monitor web traffic and maintain app health for all users. No personally identifiable information is collected.</p>
+<p>This website features a "save" feature that allows users to save classes to "lists" for future reference and planning. In order for this feature to work, we use your web browser's built-in storage functionality. We do not upload or have access to any of the data you enter into this app.</p>
+<p>Other basic information may be logged to monitor usage and maintain app health for all users. No personal information is collected.</p>
 <h3>MVLA Data Compliance</h3>
-<p>Because we do not collect any personally identifiable information, we comply with the MVLA district policy on data management.</p>
+<p>Because we do not collect any personal information, we comply with the all established precedents set by the district on data management. All information on this site is sourced from official and public documents, such as the course handbook and course offering list, or from teachers who have consented to provide alternate data and media.</p>
+<h3>Contact</h3>
+<p>If you have feedback, please reach out to us at <a href="https://get.lahs.club/help">the LAHS GET support form</a>.</p>
+<br>
 
 <style>
 	p {
 		line-height: 2;
+		font-size: 0.85em;
 	}
 </style>
