62964f5d49ed319f5690718c6352da9a864f86b3
SDBagel <sdbagel@gmail.com>
2022-01-10 20:21:52 -0800

    Abstract NavLink component

---
+++ src/lib/NavLink.svelte
@@ -0,0 +1,36 @@
+
+<script lang="ts">
+  export let href: string;
+  export let icon: string;
+</script>
+
+<a href="{href}">
+  <svg><use xlink:href="/icon/bootstrap.svg#{icon}" /></svg>
+  <slot />
+</a>
+
+<style lang="scss">
+  a {
+    border: 2px solid var(--interactable-secondary);
+    border-radius: 5px;
+    color: var(--text-primary);
+    display: flex;
+    align-items: center;
+    column-gap: 10px;
+    font-size: 0.8em;
+    text-decoration: none;
+    padding: 8px 20px;
+    transition-duration: 0.1s;
+
+    &:hover {
+      background-color: var(--interactable-primary);
+      border-color: var(--interactable-primary);
+      color: var(--text-primary);
+    }
+
+    svg {
+      height: 16px;
+      width: 16px;
+    }
+  }
+</style>
--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,6 +1,7 @@
 
 <script lang="ts">
 	import IconButton from "$lib/IconButton.svelte";
+	import NavLink from "$lib/NavLink.svelte";
 	import { createEventDispatcher } from "svelte";
 	const dispatch = createEventDispatcher();
 	const click = (e: MouseEvent) => dispatch("toggle", e);
@@ -14,34 +15,16 @@
 	<p class="sublinks">about ⋄ lahs.+ ⋄ <a href="/privacy">privacy</a></p>
 	<h3>Saved Lists</h3>
 	<div class="links">
-		<a href="">
-			<svg><use xlink:href="/icon/bootstrap.svg#bookmarks" /></svg>
-			Custom List
-		</a>
+		<NavLink href="/" icon="bookmarks">Custom List</NavLink>
 	</div>
 	<h3>Departments</h3>
 	<div class="links">
-		<a href="/">All Classes</a>
-		<a href="/elective">
-			<svg><use xlink:href="/icon/bootstrap.svg#star" /></svg>
-			Elective
-		</a>
-		<a href="/english">
-			<svg><use xlink:href="/icon/bootstrap.svg#journal" /></svg>
-			English
-		</a>
-		<a href="/art">
-			<svg><use xlink:href="/icon/bootstrap.svg#brush" /></svg>
-			Fine Art
-		</a>
-		<a href="/math">
-			<svg><use xlink:href="/icon/bootstrap.svg#calculator" /></svg>
-			Math
-		</a>
-		<a href="/science">
-			<svg><use xlink:href="/icon/bootstrap.svg#lightbulb" /></svg>
-			Science
-		</a>
+		<NavLink href="/" icon="">All Classes</NavLink>
+		<NavLink href="/elective" icon="star">Elective</NavLink>
+		<NavLink href="/english" icon="journal">English</NavLink>
+		<NavLink href="/art" icon="brush">Fine Art</NavLink>
+		<NavLink href="/math" icon="calculator">Math</NavLink>
+		<NavLink href="/science" icon="lightbulb">Science</NavLink>
 	</div>
 </nav>
 
@@ -71,29 +54,5 @@
 		flex-direction: column;
 		row-gap: 10px;
 		margin-top: 15px;
-
-		a {
-			border: 2px solid var(--interactable-secondary);
-			border-radius: 5px;
-			color: var(--text-primary);
-			display: flex;
-			align-items: center;
-			column-gap: 10px;
-			font-size: 0.8em;
-			text-decoration: none;
-			padding: 8px 20px;
-			transition-duration: 0.1s;
-
-			&:hover {
-				background-color: var(--interactable-primary);
-				border-color: var(--interactable-primary);
-				color: var(--text-primary);
-			}
-
-			svg {
-				height: 16px;
-				width: 16px;
-			}
-		}
 	}
 </style>
