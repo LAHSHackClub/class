2625862659853627f7e726a3030d4b63dc292b24
SDBagel <sdbagel@gmail.com>
2022-01-14 13:06:40 -0800

    Add active bookmark icon

--- src/lib/Class.svelte
+++ src/lib/Class.svelte
@@ -1,11 +1,15 @@
 
 <script lang="ts">
+  import { bookmarks, addBookmark } from "../util/bookmarks";
   import { createEventDispatcher } from "svelte";
   import { popoverContent } from "../util/popover";
+
   export let c: any;
   export let cList: any[];
   export let dept: string;
   export let highlight: number;
+
+  $: isBookmarked = $bookmarks.some(b => b === c.id);
   $: prereqs = c.Prerequisite ? c.Prerequisite.map(p => cList.find(c => c.id === p.id)) : [];
 
   const dispatch = createEventDispatcher();
@@ -13,6 +17,7 @@
     dispatch("selected", { id: c.id });
   }
   function alerter() {
+    addBookmark(c.id);
     popoverContent.set({
       message: "Saved " + c.Name,
       icon: "bookmarkcheck"
@@ -23,7 +28,12 @@
 <label for="{c.id}" on:dblclick="{alerter}">
   <input id="{c.id}" name="{dept}" type="radio" on:click="{selected}">
   <article class="hl-{highlight}">
-    <h3>{c.Name}</h3>
+    <h3>
+      <span>{c.Name}</span>
+      {#if isBookmarked}
+      <svg><use xlink:href="/icon/bootstrap.svg#bookmarkcheck" /></svg>
+      {/if}
+    </h3>
     <p>{c.Description}</p>
     {#if prereqs.length > 0}
     <p class="rr">
@@ -71,8 +81,16 @@
     }
 
     h3 {
+      display: flex;
+      justify-content: space-between;
       margin: 0;
       text-transform: uppercase;
+
+      svg {
+        padding: 3px 0;
+        height: 18px;
+        width: 18px;
+      }
     }
     p {
       color: var(--text-secondary);
--- src/util/bookmarks.ts
+++ src/util/bookmarks.ts
@@ -3,9 +3,11 @@ import { writable } from "svelte/store";
 
 export const bookmarks = writable<string[]>([]);
 
-function addBookmark(id: string) {
+export function addBookmark(id: string) {
   bookmarks.update(b => {
-    b.push(id);
+    if (b.includes(id))
+      return b;
+    else b.push(id);
     return b;
   });
 }
