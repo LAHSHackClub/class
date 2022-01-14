74a5235639b43f42b6fb3048bd5ebd2aaed54e81
SDBagel <sdbagel@gmail.com>
2022-01-14 12:33:20 -0800

    Add bookmark function

--- src/util/bookmarks.ts
+++ src/util/bookmarks.ts
@@ -1,4 +1,11 @@
 
 import { writable } from "svelte/store";
 
-export const bookmarks = writable([]);
+export const bookmarks = writable<string[]>([]);
+
+function addBookmark(id: string) {
+  bookmarks.update(b => {
+    b.push(id);
+    return b;
+  });
+}
