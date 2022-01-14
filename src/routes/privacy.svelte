5af9ba8b685c45761b7d8f9dd1a4a64dbf47e42a
SDBagel <sdbagel@gmail.com>
2022-01-14 15:37:42 -0800

    Tone down the snarky paragraph's snark

--- src/routes/privacy.svelte
+++ src/routes/privacy.svelte
@@ -7,9 +7,7 @@
 <p>If you are interested in learning more about us, <a href="https://get.lahs.club">check out our GET program</a> or <a href="https://lahs.club">our main page.</a></p>
 <h3>App Data Usage</h3>
 <p>This website features a "save" feature that allows users to save classes to "lists" for future reference and planning. In order for this feature to work, we use your web browser's built-in storage functionality. We do not upload or have access to any of the data you enter into this app.</p>
-<p>Other basic information may be logged to monitor usage and maintain app health for all users. No personal information is collected.</p>
-<h3>MVLA Data Compliance</h3>
-<p>Because we do not collect any personal information, we comply with the all established precedents set by the district on data management. All information on this site is sourced from official and public documents, such as the course handbook and course offering list, or from teachers who have consented to provide alternate data and media.</p>
+<p>Other basic information may be logged to monitor usage and maintain app health for all users. No personal information is collected. Data on this site was obtained through public documents and teachers who have consented to provide additional media.</p>
 <h3>Contact</h3>
 <p>If you have feedback, please reach out to us at <a href="https://get.lahs.club/help">the LAHS GET support form</a>.</p>
 <br>
