a8d002e727486b0b5c1fd718319696eaa49c80b5
SDBagel <sdbagel@gmail.com>
2022-01-16 15:59:38 -0800

    Update database.ts

--- src/util/database.ts
+++ src/util/database.ts
@@ -1,17 +1,29 @@
 
 const db = [
-  "9ef0d1b5876b479f97530f714f126c32",
+  "6cdd550534224d4bab43b114d713eda6",
+  "1cbfc877f4874cb1815846806321b843",
   "e1ed77c8a9c742489606298da0b735ab",
   "11520001890f4148abd044d31d31f1e8",
-  "0f53e05965194b8bac763c5150359ed8"
+  "6142dcf0b3f94950b1d3d6a242436c32",
+  "0f53e05965194b8bac763c5150359ed8",
+  "3b2cf319b50b48ffacf79f05093f5a68",
+  "003583aa4f48480c929fe77bc959b9e1",
+  "9ef0d1b5876b479f97530f714f126c32",
+  "7ecde15260384b4081138bc551a58009",
 ];
 
 function getDatabaseID(pathway: string) {
   switch(pathway) {
-    case "art": return "9ef0d1b5876b479f97530f714f126c32";
+    case "athletics": return "6cdd550534224d4bab43b114d713eda6";
+    case "elective": return "1cbfc877f4874cb1815846806321b843";
     case "english": return "e1ed77c8a9c742489606298da0b735ab";
     case "math": return "11520001890f4148abd044d31d31f1e8";
+    case "performing-arts": return "6142dcf0b3f94950b1d3d6a242436c32";
     case "science": return "0f53e05965194b8bac763c5150359ed8";
+    case "social-studies": return "3b2cf319b50b48ffacf79f05093f5a68";
+    case "ted": return "003583aa4f48480c929fe77bc959b9e1";
+    case "visual-arts": return "9ef0d1b5876b479f97530f714f126c32";
+    case "world-language": return "7ecde15260384b4081138bc551a58009";
   }
 }
 
