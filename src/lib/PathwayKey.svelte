6306891b6428deea9dbdaca61040e40c7a2cff18
SDBagel <sdbagel@gmail.com>
2022-01-16 19:00:00 -0800

    Change default instructions display behavior

--- src/lib/PathwayKey.svelte
+++ src/lib/PathwayKey.svelte
@@ -1,7 +1,7 @@
 
 <script lang="ts">
   import { fade } from "svelte/transition";
-  export let displayInstructions = true;
+  export let displayInstructions = false;
 </script>
 
 <div class="key-wrapper">
